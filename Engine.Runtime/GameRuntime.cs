b609e341cad3126429c6b942e7836e181e052116
Captain Cobbs <egamezhq@gmail.com>
2026-08-31 01:09:26 -0600

    Added runtime project

---
+++ Engine.Runtime/GameRuntime.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+using Engine.Core.Services;
+
+namespace Engine.Runtime
+{
+    public sealed class GameRuntime : IDisposable
+    {
+        public GameRuntime(ServiceEnvironment environment, IEnumerable<Assembly> assemblies)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TimeSpan elapsed)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>() where T : class, IService
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
